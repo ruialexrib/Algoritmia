dc882de3e6294640fa234c8cae5ccca8acac8378
Rui Ribeiro <ruialexrib@gmail.com>
2021-06-03 17:48:24 +0100

    added some coments to main

--- c++/AlgoritmosArrays/Main.cpp
+++ c++/AlgoritmosArrays/Main.cpp
@@ -11,12 +11,15 @@ int main()
 	// create array
 	int arr[5] = {};
 
+	// fill array
 	int* f_arr;
 	f_arr = Fill(arr, 5, 0, 20);
 
+	// sort array
 	int* s_arr;
 	s_arr = InsertionSort(f_arr, 5);
 
+	// print array
 	Print(s_arr, 5);
 }
 
